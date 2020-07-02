3666ac0b725e4244fadcf48ccac5e7e15e8c7bb3
Evan Feenstra <evan@evanfeenstra.com>
2020-07-02 12:03:47 -0700

    chatsnap

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Footer from './footer'
+
+function App() {
+  return (
+    <main class="main">
+  
+      <header>
+        <img src="/gorrilla.jpg" alt="logo" />
+        <span>Chat App</span>
+      </header>
+
+      <Footer />
+
+    </main>
+  )
+}
+
+export default App;
