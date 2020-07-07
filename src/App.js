cc73c4bb942536b65c6b72c7dafe1ef3745d2fe6
Evan Feenstra <evan@evanfeenstra.com>
2020-07-07 09:11:46 -0700

    message component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Footer from './footer'
+import Message from './message'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <img src="/gorrilla.jpg" alt="logo" />
         <span>Chat App</span>
       </header>
+
+      <Message />
 
       <Footer />
 
---
+++ src/message.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+function Message(){
+  return <div className="msg">
+    <div className="msg-bubble">
+      hello this is a message
+    </div>
+  </div>
+}
+
+export default Message
